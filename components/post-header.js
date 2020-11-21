0030d4f09b1673c54f777d0f7491b7bf5d28ffa1
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-11-21 01:19:17 +0100

    Removed unnecessary code

--- components/post-header.js
+++ components/post-header.js
@@ -2,7 +2,7 @@ import DateFormatter from '../components/date-formatter'
 import CoverImage from '../components/cover-image'
 import PostTitle from '../components/post-title'
 
-export default function PostHeader({ title, coverImage, date, author }) {
+export default function PostHeader({ title, coverImage, date }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
