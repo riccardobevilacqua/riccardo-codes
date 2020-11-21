dbb4a73633548109d9b74e503c3770811b086c26
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-11-21 02:47:03 +0100

    Updated intro

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
+      <div>
         <main>{children}</main>
       </div>
     </>
