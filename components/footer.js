44a1edf19c61757c4996c97644ae5862378f9a3d
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-11-25 14:35:56 +0100

    Added link to rss

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,10 @@
-import { BLOG_AUTHOR, BLOG_AUTHOR_LINK } from '@lib/constants'
+import { BLOG_AUTHOR, BLOG_AUTHOR_LINK, BLOG_URL } from '@lib/constants'
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>Created with &hearts; by <a href={BLOG_AUTHOR_LINK} target="_blank">{BLOG_AUTHOR}</a> &copy; 2020</p>
+        <p>Created with &hearts; by <a href={BLOG_AUTHOR_LINK} target="_blank">{BLOG_AUTHOR}</a> &copy; 2020 | <a href="/rss.xml">RSS</a></p>
       </div>
     </footer>
   )
